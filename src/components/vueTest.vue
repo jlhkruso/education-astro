b4057e48b393566eb161f8b0aa883c4f2054198d
Jean Loberg Hansen <jlh@kruso.dk>
2024-02-05 15:30:11 +0100

    Add vue integration and simple example.

---
+++ src/components/vueTest.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-slate-100">
+    <h1>VUE component</h1>
+    <p>{{ myRef }}</p>
+    <div>
+      <button @click="increment()">increment</button>
+    </div>
+    <button @click="decrement()">decrement</button>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+const myRef = ref(0);
+
+const increment = () => myRef.value++;
+
+const decrement = () => myRef.value--;
+</script>
